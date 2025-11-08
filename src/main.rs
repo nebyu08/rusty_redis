81d4f82fe8c384de97d2910b582a8803f55608dc
nebyu08 <nebiyuyouhannes08@gmail.com>
2025-11-08 18:37:21 +0300

    cargo V0

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ mod handle_connection;
 mod export_type;
 mod serial_deserial;
 mod db_ops;
+
 use crate::db_ops::start_db_thread;
 use crate::handle_connection::handle_client;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:6381").await?;
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
  
     let db_sender=start_db_thread("snapshot.rdb");
     let db_sender=Arc::new(db_sender);
