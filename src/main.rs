636654bb828d64ce34d1fadf40dafbf4afac519f
nebyu08 <nebiyuyouhannes08@gmail.com>
2025-11-09 18:12:11 +0300

    check point

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use std::io::{self, Write};
 use std::sync::Arc;
 use tokio::net::TcpListener;
-use std::io::{self,Write};
-mod hash_operations;
-mod handle_connection;
-mod export_type;
-mod serial_deserial;
 mod db_ops;
+mod export_type;
+mod handle_connection;
+mod hash_operations;
+mod serial_deserial;
 
 use crate::db_ops::start_db_thread;
 use crate::handle_connection::handle_client;
@@ -14,18 +14,19 @@ use crate::handle_connection::handle_client;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     eprintln!("supppppppp");
     io::stdout().flush().unwrap();
+
     match TcpListener::bind("127.0.0.1:6381").await {
         Ok(listener) => {
             eprintln!("Server started at 127.0.0.1:6381");
-         
-            let db_sender=start_db_thread("snapshot.rdb");
-            let db_sender=Arc::new(db_sender);
-        
+
+            let db_sender = start_db_thread("snapshot.rdb");
+            let db_sender = Arc::new(db_sender);
+
             loop {
                 let (socket, _) = listener.accept().await?;
                 // let db_clone = Arc::clone(&db);
-                let db_sender_clone=db_sender.clone();
-        
+                let db_sender_clone = db_sender.clone();
+
                 tokio::spawn(async move {
                     handle_client(socket, db_sender_clone)
                         .await
@@ -33,10 +34,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 });
             }
         }
-        Err(e)=>{
-            eprint!("Error: {}", e);
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
         }
     }
-    Ok(())
-
+    // Ok(())
 }
